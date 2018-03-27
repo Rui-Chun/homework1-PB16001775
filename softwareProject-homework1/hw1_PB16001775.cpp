dee6da174022a39358673752a73b7a423e72907f
Rui-Chun <gymrc@mail.ustc.edu.cn>
2018-03-28 04:34:04 +0800

    zero版本

--- softwareProject-homework1/hw1_PB16001775.cpp
+++ softwareProject-homework1/hw1_PB16001775.cpp
@@ -71,35 +71,7 @@ void GetAllFiles(string path, vector<string>& files)
 
 }
 
-//获取特定格式的文件名    
-void GetAllFormatFiles(string path, vector<string>& files, string format)
-{
-	//文件句柄      
-	long   hFile = 0;
-	//文件信息      
-	struct _finddata_t fileinfo;
-	string p;
-	if ((hFile = _findfirst(p.assign(path).append("\\*" + format).c_str(), &fileinfo)) != -1)
-	{
-		do
-		{
-			if ((fileinfo.attrib &  _A_SUBDIR))
-			{
-				if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
-				{
-					//files.push_back(p.assign(path).append("\\").append(fileinfo.name) );    
-					GetAllFormatFiles(p.assign(path).append("\\").append(fileinfo.name), files, format);
-				}
-			}
-			else
-			{
-				files.push_back(p.assign(path).append("\\").append(fileinfo.name));
-			}
-		} while (_findnext(hFile, &fileinfo) == 0);
 
-		_findclose(hFile);
-	}
-}
 
 void addWord(string &word, string &word_pre, string &word_r, string &word_pre_r)
 {
